683299b7cfa028546b931af0ee163d65c4b9dc9d
Rob <robertoles@me.com>
2010-09-02 13:29:30 +0100

    Adapting connection to look for mongohq_url and setup a connection from that

--- lib/sinatra/mongoid.rb
+++ lib/sinatra/mongoid.rb
@@ -4,17 +4,23 @@ require 'mongoid'
 module Sinatra
   module MongoidExtension
     def self.registered(app)
-      app.set :mongo_host,     ENV['MONGOID_HOST']      || 'localhost'
-      app.set :mongo_db,       ENV['MONGOID_DATABASE']  || 'changeme'
-      app.set :mongo_port,     ENV['MONGOID_PORT']      || Mongo::Connection::DEFAULT_PORT
-      app.set :mongo_user,     ENV['MONGOID_USERNAME']
-      app.set :mongo_password, ENV['MONGOID_PASSWORD']
+      if ENV['MONGOHQ_URL']
+        uri = URI.parse(ENV['MONGOHQ_URL'])
+        conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+        Mongoid.database = conn.db(uri.path.gsub(/^\//, ''))
+      else
+        app.set :mongo_host,     ENV['MONGOID_HOST']      || 'localhost'
+        app.set :mongo_db,       ENV['MONGOID_DATABASE']  || 'changeme'
+        app.set :mongo_port,     ENV['MONGOID_PORT']      || Mongo::Connection::DEFAULT_PORT
+        app.set :mongo_user,     ENV['MONGOID_USERNAME']
+        app.set :mongo_password, ENV['MONGOID_PASSWORD']
 
-      Mongoid.database = Mongo::Connection.new(app.mongo_host,app.mongo_port).
-                                           db(app.mongo_db)
-      if app.mongo_user
-        Mongoid.database.authenticate(app.mongo_user,
-                                      app.mongo_password)
+        Mongoid.database = Mongo::Connection.new(app.mongo_host,app.mongo_port).
+                                             db(app.mongo_db)
+        if app.mongo_user
+         Mongoid.database.authenticate(app.mongo_user,
+                                       app.mongo_password)
+        
       end
     end
   end
